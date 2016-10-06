34047863f5599e88df74eed7f36e1d16f44bcc10
Propupul <luisvalencia3@hotmail.com>
2016-10-06 11:30:21 -0400

    Updates

--- palletstore.py
+++ palletstore.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request
 from flask import flash
 # from flask_script import Manager
 from flask_bootstrap import Bootstrap
