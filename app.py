048934053b60e7371edaa63362644c80e8b4daa5
Propupul <luisvalencia3@hotmail.com>
2017-02-12 14:55:50 -0500

    Updated

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, url_for, redirect, request, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_wtf import Form
 from datetime import datetime, date
-from database_config import PrimaryView, db
-from wtforms import SubmitField, SelectField
+from models import StoreRetrieve, db
+from nameform import NameForm
 '''Imported modules'''
 
 app = Flask(__name__)
@@ -16,26 +15,6 @@ app.config['SECRET_KEY'] = 'some very hard to guess key'
 '''SECRET_KEY will be moved'''
 
 
-class NameForm(Form):
-    select = SelectField('Select Product', choices=[
-                         ('464 50lbs', '464 50lbs'),
-                         ('444 50lbs', '444 50lbs'),
-                         ('464 50lbs banded', '464 50lbs banded'),
-                         ('464 10lbs', '464 10lbs'),
-                         ('8oz Mason', '8oz Mason'),
-                         ('4oz Jelly', '4oz Jelly'),
-                         ('6oz Tin 120', '6oz Tin 120'),
-                         ('4oz Tin 120', '4oz Tin 120'),
-                         ('Soy Candle Making Kit', 'Soy Candle Making Kit'),
-                         ('4oz candle tin 12pc', '4oz candle tin 12pc'),
-                         ('8oz Mason MP', '8oz Mason MP')
-                         ])
-    submit = SubmitField('Save')
-'''class NameForm creates an object inheriting from Form.
-Using SelectField creates a dropdown which takes two arguments,
-that make it a dropdown and SubmitField to create a save button'''
-
-
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html', current_time=datetime.utcnow()), 404
@@ -43,14 +22,13 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return (render_template('500.html', current_time=datetime.utcnow()), 500)
-'''Two error pages that will be moved to a different file with the Blueprint
-object'''
+    return render_template('500.html', current_time=datetime.utcnow()), 500
+'''Two error pages '''
 
 
 @app.route('/')
 def index():
-    order = PrimaryView.query.order_by(PrimaryView.date).all()
+    order = StoreRetrieve.query.order_by(StoreRetrieve.date).all()
     return render_template('index.html', current_time=datetime.utcnow(),
                            order=order)
 
@@ -58,8 +36,8 @@ def index():
 @app.route('/retrieve', methods=['GET', 'POST'])
 def retrieve():
     form = NameForm()
-    order = PrimaryView.query.filter_by(sku=form.select.data).order_by(PrimaryView.date).all()
-    older_date = PrimaryView.query.filter_by(sku=form.select.data).order_by(PrimaryView.date).first()
+    order = StoreRetrieve.query.filter_by(sku=form.select.data).order_by(StoreRetrieve.date).all()
+    older_date = StoreRetrieve.query.filter_by(sku=form.select.data).order_by(StoreRetrieve.date).first()
     for item in order:
         if older_date.date <= str(date.today()) and request.method == 'POST':
             perma_date = older_date.date
@@ -75,15 +53,15 @@ def retrieve():
             return(redirect(url_for('retrieve')))
     return render_template('retrieve.html',
                            current_time=datetime.utcnow(), form=form)
-''''Retrieve Page: Retrieves the selected product with the oldest date. stored the NameForm object in the "form" variable. "order" variable holds PrimaryView which is the name of the database model, imorted from database_config.py which sets up the initial configuration using flask-sqlalchemy. "order" object variable queries the entire list of products in their location, filtered by selected proudct, and ordered by date. "older_date" object variable queries the first item/sku/product in the list by selected product, and by date. for "item" in "order" iterates through the entire list of products using "item" as the index variable. if "older_date.date" is less than today's date and the "request.method" is equals to 'POST' then create a "perma_date" variable containing the "older_date.date" object. Make the "older_date.sku" an "Empty" string, and the "older.date.date" a "N/A" string as well. Add and commit the session to the table. Used flask-bootstrap and give the user a "flash" message which displays the product name and its store date. If locations are full display a "there are no more pallets avialble to retrieve DEBUG" thats' a debug for me.  '''
+''''Retrieve Page: Retrieves the selected product with the oldest date. stored the NameForm object in the "form" variable. "order" variable holds StoreRetrieve which is the name of the database model, imorted from database_config.py which sets up the initial configuration using flask-sqlalchemy. "order" object variable queries the entire list of products in their location, filtered by selected proudct, and ordered by date. "older_date" object variable queries the first item/sku/product in the list by selected product, and by date. for "item" in "order" iterates through the entire list of products using "item" as the index variable. if "older_date.date" is less than today's date and the "request.method" is equals to 'POST' then create a "perma_date" variable containing the "older_date.date" object. Make the "older_date.sku" an "Empty" string, and the "older.date.date" a "N/A" string as well. Add and commit the session to the table. Used flask-bootstrap and give the user a "flash" message which displays the product name and its store date. If locations are full display a "there are no more pallets avialble to retrieve DEBUG" thats' a debug for me.  '''
 
 
 @app.route('/store', methods=['GET', 'POST'])
 def store():
     form = NameForm()
-    view = PrimaryView.query.all()
+    view = StoreRetrieve.query.all()
     for item in view:
-        sku_search = PrimaryView.query.filter_by(sku='Empty').first()
+        sku_search = StoreRetrieve.query.filter_by(sku='Empty').first()
         try:
             if sku_search.sku == 'Empty' and request.method == 'POST':
                 sku_search.sku = form.select.data
---
+++ nameform.py
@@ -0,0 +1,22 @@
+from wtforms import SubmitField, SelectField
+from flask_wtf import Form
+
+
+'''class NameForm creates an object inheriting from Form.
+Using SelectField creates a dropdown which takes two arguments,
+that make it a dropdown and SubmitField to create a save button'''
+class NameForm(Form):
+    select = SelectField('Select Product', choices=[
+                         ('464 50lbs', '464 50lbs'),
+                         ('444 50lbs', '444 50lbs'),
+                         ('464 50lbs banded', '464 50lbs banded'),
+                         ('464 10lbs', '464 10lbs'),
+                         ('8oz Mason', '8oz Mason'),
+                         ('4oz Jelly', '4oz Jelly'),
+                         ('6oz Tin 120', '6oz Tin 120'),
+                         ('4oz Tin 120', '4oz Tin 120'),
+                         ('Soy Candle Making Kit', 'Soy Candle Making Kit'),
+                         ('4oz candle tin 12pc', '4oz candle tin 12pc'),
+                         ('8oz Mason MP', '8oz Mason MP')
+                         ])
+    submit = SubmitField('Save')
